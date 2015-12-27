1849bd60068737e1f2edcbc48a454d7ceccd5beb
Ben Reeves <benwolverine2019@gmail.com>
2015-12-26 19:08:26 -0500

    Added Operator and Solution classes
    Implemented Operator, stubbed out Solution

--- brute_force_algorithm.py
+++ brute_force_algorithm.py
@@ -3,6 +3,53 @@ This is the brute-force way of finding a solution to a 24 Card.
 It just tries every possible combination to find a solution.
 """
 
+class Operator(object):
+    """
+    Represents an operator ('*', '+', '-', '/') used in solving a 24 Card.
+    """
+    def __init__(self, op):
+        self.op = op
+
+    def evaluate(self, left, right):
+        """
+        Evaluates the result of multiplying/adding/subtracting/dividing left and right
+        :param left: The left operand
+        :param right: The right operand
+        :return: The result of executing the operator on the two operands
+        """
+        if self.op == '*':
+            return left * right
+        elif self.op == '+':
+            return left + right
+        elif self.op == '-':
+            return left - right
+        elif self.op == '/':
+            return left / right
+        else:
+            return "Error"
+
+class Solution(object):
+    """
+    Represents a potential solution to a 24 Card.
+    Has an array of 4 numbers and an array of 3 operations.
+    A Solution does not necessarily have to be correct.
+    """
+    numbers = []
+    operations = []
+
+    def __init__(self):
+        pass
+
+    def evaluate(self):
+        """
+        Evaluates the result of this Solution.
+        Executes the 3 operations (in order) on the 4 numbers (in order).
+        num1 <op1> num2 <op2> num3 <op3> num4
+        :return: The result of evaluating the Solution.
+        """
+        return 0
+
+
 # The 24 card. It's an array of 4 numbers
 card = []
 
