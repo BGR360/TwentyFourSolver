5089bbf65d48d1e6d7411c908aece5114d73be0d
Ben Reeves <benwolverine2019@gmail.com>
2015-12-27 16:26:56 -0500

    Copied some code from brute force algorithm

--- my_algorithm.py
+++ my_algorithm.py
@@ -30,28 +30,156 @@ Given n numbers (stored in array A), can you use arithmetic to arrive at x?
     a)  If A[0] = x, then yes
     b)  If A[0] != x, then no
 
-2.  If there are factors of x in A, pick one.
+2.  Try adding all the numbers in A together.
+
+3.  Try multiplying all the numbers in A together.
+
+4.  If there are factors of x in A, pick one.
     * Prefer 1 as a factor of x, and then prefer smaller factors
     a)  See if the other n - 1 numbers can make the other factor of x
         i)  If so, then yes
         ii) If not, then pick another factor
 
-3.  If there are no more factors of x in A, then for each number a in A:
+5.  If there are no more factors of x in A, then for each number a in A:
     a)  SUBTRACT a from x
     b)  See if the other n - 1 numbers can form the result
         i)  If so, then yes
         ii) If not, try the next number
 
-4.  If that fails, then for each number a in A:
+6.  If that fails, then for each number a in A:
     a)  ADD a to x
     b)  See if the other n - 1 numbers can form the result
         i)  If so, then yes
         ii) If not, try the next number
 
-5.  If that fails, then for each number a in A:
+7.  If that fails, then for each number a in A:
     a)  MULTIPLY x by a
     b)  See if the other n - 1 numbers can form the result
         i)  If so, then yes
         ii) If not, then no solution
 """
 
+class Operator(object):
+    """
+    Represents an operator ('*', '+', '-', '/') used in solving a 24 Card.
+    """
+    def __init__(self, op):
+        self.op = op
+
+    def evaluate(self, left, right):
+        """
+        Evaluates the result of multiplying/adding/subtracting/dividing left and right
+        :param left: The left operand
+        :param right: The right operand
+        :return: The result of executing the operator on the two operands
+        """
+        if self.op == '*':
+            return left * right
+        elif self.op == '+':
+            return left + right
+        elif self.op == '-':
+            return left - right
+        elif self.op == '/':
+            return left / right
+        else:
+            return "Error"
+
+    def __repr__(self):
+        return str(self.op)
+
+class Solution(object):
+    """
+    Represents a potential solution to a 24 Card.
+    Has an array of 4 numbers and an array of 3 operations.
+    A Solution does not necessarily have to be correct.
+    """
+    numbers = []
+    operations = []
+
+    def __init__(self):
+        pass
+
+    def evaluate(self):
+        """
+        Evaluates the result of this Solution.
+        Executes the 3 operations (in order) on the 4 numbers (in order).
+        num1 <op1> num2 <op2> num3 <op3> num4
+        :return: The result of evaluating the Solution.
+        """
+        result = self.numbers[0]
+        for i in range(1, len(self.numbers)):
+            left = result
+            right = self.numbers[i]
+            operator = self.operations[i - 1]
+            result = operator.evaluate(left, right)
+        return result
+
+    def __repr__(self):
+        """
+        Makes a human-readable string to represent this Solution
+        :return The string representation of this Solution
+        """
+        result = str(self.numbers[0])
+        for i in range(1, len(self.numbers)):
+            op = self.operations[i - 1].op
+            num = self.numbers[i]
+            result += " " + op + " " + str(num)
+        return result
+
+
+def is_correct(solution, value=24):
+    """
+    Checks if solution evaluates to value
+    :param solution: The Solution instance that should be checked
+    :param value: The number that we expect solution to evaluate to
+    :return: True if solution evaluates to value, False if otherwise
+    """
+    return solution.evaluate() == value
+
+
+
+def solve_card(card):
+    """
+    This method solves the 24 Card using my custom algorithm
+    :param card: An array representing the 24 Card
+    :return: Returns an array where the first element is a Solution instance if a solution was found, and False if no
+    solution was found. The second element is always the number of attempts that were made to solve it.
+    """
+
+    # Constants for the different Operators
+    MUL = Operator('*')
+    ADD = Operator('+')
+    SUB = Operator('-')
+    DIV = Operator('/')
+    OPS = [MUL, ADD, SUB, DIV]
+
+    # The number of different combinations we've tried to find the answer
+    num_attempts = 0
+    current_attempt = Solution()
+
+    # Begin my algorithm
+    return [False, num_attempts]
+
+
+
+# The 24 card. It's an array of 4 numbers
+card = []
+
+# Get the card's numbers from the user
+user_input = raw_input("Please enter 4 numbers separated by a space: ")
+split_input = user_input.split()
+for num_str in split_input:
+    card.append(int(num_str))
+
+# Solve the card
+result = solve_card(card)
+solution = result[0]
+num_attempts = result[1]
+
+# Print results
+if solution != False:
+    print "Solution: %s" % solution
+else:
+    print "No solution found."
+
+print "Number of attempts: %s" % num_attempts
