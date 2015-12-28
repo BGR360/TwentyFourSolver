edac5f8b6e0c1f8e1f76009f599544fa153debf7
Ben Reeves <benwolverine2019@gmail.com>
2015-12-27 21:53:59 -0600

    Implemented some basic testing. Not very useful.

--- compare_algorithms.py
+++ compare_algorithms.py
@@ -1,3 +1,21 @@
 """
 This script runs a multitude of tests on both algorithms to compare their performance.
 """
+
+from subprocess import call
+from itertools import combinations_with_replacement
+
+# Callable command line statements
+execute_brute_force = ['python', 'brute_force_algorithm.py']
+execute_my_algorithm = ['python', 'my_algorithm.py']
+
+# Test all possible 24 Cards
+for card in combinations_with_replacement(range(1, 14), 4):
+    # Convert to an array of strings
+    card_as_str = []
+    for num in card:
+        card_as_str.append(str(num))
+
+    # Solve the card with both algorithms
+    call(execute_brute_force + card_as_str)
+    call(execute_my_algorithm + card_as_str)
